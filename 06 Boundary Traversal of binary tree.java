4b34789380b0a9340970eb8fd4a562eebfc3bb87
Arihant Singh Verma <78214117+arihant30@users.noreply.github.com>
2021-08-01 21:20:11 +0530

    Create 06 Boundary Traversal of binary tree.java

---
+++ 06 Boundary Traversal of binary tree.java
@@ -0,0 +1,53 @@
+class Solution
+{
+	ArrayList<Integer> outputarr;
+	ArrayList <Integer> printBoundary(Node node)
+	{
+	    outputarr=new ArrayList<>();
+	    if(node==null){
+	        return outputarr;
+	    }
+	    if(node.left==null && node.right==null){
+	        outputarr.add(node.data);
+	        return outputarr;
+	    }
+	    outputarr.add(node.data);
+	    printleft(node.left);
+	    printleaves(node.left);
+	    printleaves(node.right);
+	    printright(node.right);
+	    return outputarr;
+	}
+	void printleft(Node node){
+	    if(node==null)
+	        return;
+        if(node.left!=null){
+            outputarr.add(node.data);
+            printleft(node.left);
+        }
+        else if(node.right!=null){
+            outputarr.add(node.data);
+            printleft(node.right);
+        }
+	}
+	void printleaves(Node node){
+	    if(node==null)
+	        return;
+	   printleaves(node.left);
+	   if(node.left==null && node.right==null)
+	        outputarr.add(node.data);
+	   printleaves(node.right);
+	}
+	void printright(Node node){
+	    if(node==null)
+	        return;
+	   if(node.right!=null){
+	       printright(node.right);
+	       outputarr.add(node.data);
+	   }
+	   else if(node.left!=null){
+	       printright(node.left);
+	       outputarr.add(node.data);
+	   }
+	}
+}
